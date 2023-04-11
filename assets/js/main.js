ac8ed822571e77cbdf4ec0a7a11521af4de80d17
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-11 13:56:34 +0200

    add url key and initial get axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,23 @@
+/* Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
 const { createApp } = Vue
 
 const app = createApp({
-  data() {
-    return {
-
-    }
-  },
-  methods: {
-   
+    data() {
+        return {
+            url: "https://flynn.boolean.careers/exercises/api/random/mail"
+        }
     },
-mounted() {
-},
-  
+    methods: {
+
+    },
+    mounted() {
+        axios
+        .get(this.url)
+    },
+
 })
 
 app.mount('#app')
